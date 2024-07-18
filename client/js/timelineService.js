6d9d1e0d7419b5ffa7ec3a2e798feb9f50bdb855
Chieh-Chun Lin <sukusukustudio@protonmail.com>
2024-07-18 23:01:21 +0800

    add nativeLineBtn click for line login

--- client/js/timelineService.js
+++ client/js/timelineService.js
@@ -256,6 +256,22 @@ if (window.location.href.includes("/timeline")) {
   //=====================================================
   //============  User Management Center  ===============
   //=====================================================
+  $("#nativeLineBtn").on('click', function() {
+    const LINE_CHANNEL_ID = "2005642025";
+    const LINE_CALLBACK_URI = "https://www.chiehchunlin.com/lineCallback";
+    //const LINE_CALLBACK_URI =
+    //  "https://a949-59-120-11-125.ngrok-free.app/lineCallback";
+    const LINE_STATE = crypto.randomUUID().replace(/[-]+/g, ""); //prevent xss
+    const LINE_NONCE = crypto.randomUUID().replace(/[-]+/g, ""); //prevent xss
+    let link = "https://access.line.me/oauth2/v2.1/authorize?";
+    link += "response_type=code";
+    link += "&client_id=" + LINE_CHANNEL_ID;
+    link += "&redirect_uri=" + LINE_CALLBACK_URI;
+    link += "&state=" + LINE_STATE;
+    link += "&scope=profile%20openid&nonce=" + LINE_NONCE;
+    window.location.href = link;
+  });
+
   $('#managesTitle .edit-btn').on('click', function() {
     $('#manages .delete-btn').toggle();
   });
